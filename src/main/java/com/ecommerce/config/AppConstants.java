bcf33374edd384dcf8710e09ad6a7d302bac8cb5
Nihal Singh <nihalsingh2082002@gmail.com>
2025-10-13 17:29:12 +0530

    Implemented Sorting of Categories

--- src/main/java/com/ecommerce/config/AppConstants.java
+++ src/main/java/com/ecommerce/config/AppConstants.java
@@ -4,4 +4,6 @@ public class AppConstants {
     // Why String ? ---> read this in notes folder ---> AppConstants and Default Values of RequestParam
     public static final String PAGE_NUMBER = "0";
     public static final String PAGE_SIZE = "10";
+    public static final String SORT_CATEGORIES_BY = "categoryId";
+    public static final String SORT_DIR = "asc";
 }
